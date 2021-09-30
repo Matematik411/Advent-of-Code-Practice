edb50d10c63c5fb30849f4ed559fb6a07909357f
Matematik411 <nz3928@student.uni-lj.si>
2021-10-01 00:07:22 +0200

    day03

--- AdventOfCode2018/day03.py
+++ AdventOfCode2018/day03.py
@@ -10,4 +10,25 @@ while True:
     except:
         break
 
+    id, x, y, w, h = map(int,re.match(r"#(\d*) @ (\d*),(\d*): (\d*)x(\d*)", line).groups())
+
+
+    cases[(x, y)] = (w, h, id)
+    for i in range(w):
+        for j in range(h):
+            plane[x + i][y + j] += 1
     
+
+print("a: " + str(sum([sum([1 for x in plane[j] if x > 1]) for j in range(1000)])))
+    
+for (x, y), (w, h, id) in cases.items():
+    covered = False
+
+    for i in range(w):
+        for j in range(h):
+            if plane[x + i][y + j] != 1:
+                covered = True
+    
+    if not covered:
+        print("b: " + str(id))
+        break
