6450b669b2e13697a61cdad4a891430bc74f34c6
Matematik411 <nz3928@student.uni-lj.si>
2021-09-30 21:27:41 +0200

    regex revision

---
+++ AdventOfCode2018/day03.py
@@ -0,0 +1,13 @@
+import re
+
+plane = [[0 for _ in range(1000)] for _ in range(1000)]
+cases = {}
+
+
+while True:
+    try:
+        line = input()
+    except:
+        break
+
+    
---
+++ AdventOfCode2018/test.py
@@ -0,0 +1,6 @@
+import re
+s = "#1 @ 596,731: 11x27"
+
+print(re.match(r".*@ (\d*),(\d*): (\d*)x(\d*)", s).groups())
+
+print(re.compile(r".*@ (\d*),(\d*): (\d*)x(\d*)").split(s))
