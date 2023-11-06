19de6597c72e67875129fdfc1087f2ede189e551
Nejc Zajc <nz44156@student.uni-lj.si>
2023-11-06 22:54:55 +0100

    testing c++

---
+++ AdventOfCodeWithC++/reading.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+#include <fstream>
+#include <string>
+
+int main() {
+    std::string filename;
+    filename = "text.txt";
+
+
+    std::ifstream inputFile(filename);
+
+    if (!inputFile.is_open()) {
+        std::cerr << "Error opening file." << std::endl;
+        return 1;
+    }
+
+    std::string line;
+    while (std::getline(inputFile, line)) {
+        std::cout << line << std::endl;
+    }
+
+    inputFile.close();
+
+    return 0;
+}
